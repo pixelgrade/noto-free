69f5666c86fcbf144f537d7d707a130c2c84860d
Vlad Olaru <vlad.olaru@pixelgrade.com>
2018-01-12 19:47:11 +0200

    removed old components

--- components/blog/template-parts/entry-header-page.php
+++
@@ -1,1 +0,0 @@
-<?php the_title( '<h1 class="entry-title u-page-title-color">', '</h1>' ); ?>
--- components/blog/template-parts/entry-thumbnail.php
+++
@@ -1,7 +0,0 @@
-<?php if ( has_post_thumbnail() ) { ?>
-
-	<div class="entry-thumbnail">
-		<div><?php the_post_thumbnail( 'single-' . pixelgrade_get_post_thumbnail_aspect_ratio_class() ); ?></div>
-	</div>
-
-<?php } ?>
