9d4e915da9bafb160dd6a3b8d65758df14d2ab70
George <george.olaru@pixelgrade.com>
2018-07-04 12:02:16 +0300

    Init #31

--- assets/js/style.bundle.js
+++ assets/js/style.bundle.js
@@ -14,7 +14,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /***/ 26:
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \n        @include cq-prolyfill(\"width < 100px\") {\n                ^\n      No mixin named cq-prolyfill\n\nBacktrace:\n\tvariations/noto/scss/_widgets.scss:26\n      in /Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/variations/noto/scss/_widgets.scss (line 26, column 18)\n    at runLoaders (/Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.<anonymous> (/Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/node_modules/sass-loader/node_modules/async/dist/async.js:2257:31)\n    at Object.callback (/Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/node_modules/sass-loader/node_modules/async/dist/async.js:958:16)\n    at options.error (/Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/node_modules/node-sass/lib/index.js:294:32)");
 
 /***/ })
 
--- templates/blog/header.php
+++ templates/blog/header.php
@@ -91,10 +91,30 @@ do_action( 'pixelgrade_before_barba_wrapper', 'main' );
         </div>
 
         <div class="c-noto c-noto--body">
+        	<div class="widget widget_text widget--content widget_custom_html">
+				<h2 class="widget__title h6">Salut</h2>
+				<div class="textwidget">
+					<p>Welcome to my blog! Check out the latest post, browse the highlights or reach me to say Hi!</p>
+				</div>
+			</div>
+
 			<div class="widget widget_text widget--content widget_custom_html">
 				<h2 class="widget__title h6">Widget Title</h2>
 				<div class="textwidget">
 					<p>The Text Widget allows you to add text to your sidebar. It’s the most popular widget because of its power and flexibility — use one to display text, links, images, or any combination.</p>
+					<p class="small">
+						Put simply, smaller text will usually be read after smaller text. Small columns of text are easier to read than large ones.
+					</p>
+				</div>
+			</div>
+
+			<div class="widget widget_text widget--content widget_custom_html">
+				<h2 class="widget__title h6">Widget Title</h2>
+				<div class="textwidget">
+					<p>The Text Widget allows you to add text to your sidebar. It’s the most popular widget because of its power and flexibility — use one to display text, links, images, or any combination.</p>
+					<p class="small">
+						Put simply, smaller text will usually be read after smaller text. Small columns of text are easier to read than large ones.
+					</p>
 				</div>
 			</div>
 	        <?php
