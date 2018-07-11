293b10f924ac548835ca2b90143e35463e8cf57f
Razvan <razwan.me@gmail.com>
2018-07-11 10:43:15 +0300

    improve display of archive badge

--- template-parts/badge.php
+++ template-parts/badge.php
@@ -5,11 +5,13 @@ $badge_content = pixelgrade_option( 'archive_badge_content' );
 if ( ! empty( $badge_title ) || ! empty( $badge_content ) ) { ?>
 	<div class="c-badge">
 		<?php if ( ! empty ( $badge_title ) ) { ?>
-			<div class="c-badge__title"><?php echo $badge_title ?></div>
+			<h6 class="c-badge__title"><?php echo $badge_title ?></h6>
 		<?php } ?>
 
 		<?php if ( ! empty ( $badge_content ) ) { ?>
 			<div class="c-badge__content"><?php echo $badge_content ?></div>
 		<?php } ?>
+
+        <?php get_template_part( 'template-parts/svg/wave-card-svg' ); ?>
 	</div>
 <?php } ?>
