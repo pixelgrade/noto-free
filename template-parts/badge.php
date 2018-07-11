1c92189055a5d3d22a7d6ff1f7a6bbffd8c42a8a
George <george.olaru@pixelgrade.com>
2018-07-11 15:15:02 +0300

    ++ Post It

--- template-parts/badge.php
+++ template-parts/badge.php
@@ -3,15 +3,18 @@ $badge_title = pixelgrade_option( 'archive_badge_title' );
 $badge_content = pixelgrade_option( 'archive_badge_content' );
 
 if ( ! empty( $badge_title ) || ! empty( $badge_content ) ) { ?>
-	<div class="c-badge widget post-it small">
-		<?php if ( ! empty ( $badge_title ) ) { ?>
-			<h6 class="c-badge__title"><?php echo $badge_title ?></h6>
-		<?php } ?>
+	<div class="c-gallery__ite  c-gallery__item--widget  c-gallery__item--post-it  post-it  small">
+		
+		<div class="widget">
+			<?php if ( ! empty ( $badge_title ) ) { ?>
+				<h6><?php echo $badge_title ?></h6>
+			<?php } ?>
 
-		<?php if ( ! empty ( $badge_content ) ) { ?>
-			<div class="c-badge__content"><?php echo $badge_content ?></div>
-		<?php } ?>
+			<?php if ( ! empty ( $badge_content ) ) { ?>
+				<div class="c-badge__content"><?php echo $badge_content ?></div>
+			<?php } ?>
+		</div>
 
-        <?php get_template_part( 'template-parts/svg/wave-card-svg' ); ?>
 	</div>
+
 <?php } ?>
--- variations/noto/synced/inc/specific/extras.php
+++ variations/noto/synced/inc/specific/extras.php
@@ -204,7 +204,8 @@ if ( ! function_exists( 'noto_alter_blog_component_config' ) ) {
 	                'sidebar_args' => array(
                 		'name'          => esc_html__( 'Posts Grid Widgets', '__components_txtd' ),
                     	'description'   => esc_html__( 'Insert your favorite widgets here, and we will place them throughout the Frontpage posts grid.', '__components_txtd' ),
-	                    'before_widget' => '<section id="%1$s" class="widget widget--misto c-gallery__item %2$s">',
+	                    'before_widget' => '<div class="c-gallery__item c-gallery__item--widget %2$s"><section id="%1$s" class="widget">',
+	                    'after_widget' => '</section></div>',
                     ),
                 ),
 				'sidebar-2' => array(
