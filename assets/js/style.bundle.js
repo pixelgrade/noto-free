c003f31059bd5888f9d5c30909b10a0c409f4d31
George <george.olaru@pixelgrade.com>
2018-07-24 18:26:51 +0300

    Auto-styling first paragraph?

--- assets/js/style.bundle.js
+++ assets/js/style.bundle.js
@@ -14,7 +14,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /***/ 27:
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \n.single,\n^\n      Invalid CSS after '...graph: \".intro\"': expected expression (e.g. 1px, bold), was \".single,\"\n      in /Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/variations/noto/scss/_single.scss (line 3, column 1)\n    at runLoaders (/Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.<anonymous> (/Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/node_modules/sass-loader/node_modules/async/dist/async.js:2257:31)\n    at Object.callback (/Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/node_modules/sass-loader/node_modules/async/dist/async.js:958:16)\n    at options.error (/Users/georgeolaru/Local Sites/demos/app/public/wp-content/themes/noto/node_modules/node-sass/lib/index.js:294:32)");
 
 /***/ })
 
