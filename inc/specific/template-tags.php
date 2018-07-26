549034eb273ee9089e9a07bd6a866d33734dd4c4
Razvan <razwan.me@gmail.com>
2018-07-26 16:38:54 +0300

    working pattern style selector

--- inc/specific/template-tags.php
+++ inc/specific/template-tags.php
@@ -126,28 +126,26 @@ if ( ! function_exists( 'noto_get_pattern_svg' ) ) {
 
 	function noto_get_pattern_svg( $color = 'currentColor', $pattern = 'wave' ) {
 		ob_start();
-		?>
 
-	    <?php if ( 'triangle' === $pattern ) { ?>
-			<svg width="25px" height="8px" viewBox="0 0 25 8" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-		        <g id="triangle" stroke="none" stroke-width="1" fill="<?php echo $color; ?>" fill-rule="evenodd">
-		            <polygon points="0 0 4.5 8 9 0"></polygon>
-		        </g>
-			</svg>
-	    <?php } ?>
+		switch ( $pattern ) {
+		    case "triangles": ?>
+				<svg width="25px" height="8px" viewBox="0 0 25 8" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+			        <g id="triangle" stroke="none" stroke-width="1" fill="<?php echo $color; ?>" fill-rule="evenodd">
+			            <polygon points="0 0 4.5 8 9 0"></polygon>
+			        </g>
+				</svg>
+		        <?php break;
+		    default: ?>
+			    <svg width="18px" height="8px" viewBox="0 0 18 8" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+			        <g id="wave" transform="translate(-12, 1)" fill="none" stroke="<?php echo $color; ?>" stroke-width="2">
+			            <path d="M8,3 C8.66808837,5 10.0010521,6 11.9988911,6 C13.9967301,6 15.3304331,5 16,3"></path>
+			            <path d="M16,0 C16.6680884,2 18.0010521,3 19.9988911,3 C21.9967301,3 23.3304331,2 24,0" transform="translate(20, 1.5) rotate(-180) translate(-20, -1.5) "></path>
+			            <path d="M24,3 C24.6680884,5 26.0010521,6 27.9988911,6 C29.9967301,6 31.3304331,5 32,3"></path>
+			            <path d="M0,0 C0.668088373,2 2.00105206,3 3.99889107,3 C5.99673007,3 7.33043305,2 8,0" transform="translate(4, 1.5) rotate(-180) translate(-4, -1.5) "></path>
+			        </g>
+				</svg>
+		<?php }
 
-	    <?php if ( 'wave' === $pattern ) { ?>
-			<svg width="18px" height="8px" viewBox="0 0 18 8" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-		        <g id="wave" transform="translate(-12, 1)" fill="none" stroke="<?php echo $color; ?>" stroke-width="2">
-		            <path d="M8,3 C8.66808837,5 10.0010521,6 11.9988911,6 C13.9967301,6 15.3304331,5 16,3"></path>
-		            <path d="M16,0 C16.6680884,2 18.0010521,3 19.9988911,3 C21.9967301,3 23.3304331,2 24,0" transform="translate(20, 1.5) rotate(-180) translate(-20, -1.5) "></path>
-		            <path d="M24,3 C24.6680884,5 26.0010521,6 27.9988911,6 C29.9967301,6 31.3304331,5 32,3"></path>
-		            <path d="M0,0 C0.668088373,2 2.00105206,3 3.99889107,3 C5.99673007,3 7.33043305,2 8,0" transform="translate(4, 1.5) rotate(-180) translate(-4, -1.5) "></path>
-		        </g>
-			</svg>
-	    <?php } ?>
-
-		<?php
 		$svg = ob_get_clean();
 		return $svg;
 	}
@@ -156,7 +154,7 @@ if ( ! function_exists( 'noto_get_pattern_svg' ) ) {
 if ( ! function_exists( 'noto_get_pattern_background_image' ) ) {
 
 	function noto_get_pattern_background_image( $color = '', $pattern = 'wave' ) {
-		$pattern = pixelgrade_option( 'noto_decoration_pattern', 'wave' );
+		$pattern = pixelgrade_option( 'pattern_style', 'wave' );
 
 		if ( empty( $color ) ) {
 			$color = pixelgrade_option( 'main_content_body_text_color', '#49494B' );
