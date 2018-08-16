527fde03ab5ca1dcf9328710aa4cebe8734b317a
Vlad Olaru <vlad.olaru@pixelgrade.com>
2018-08-16 14:08:40 +0300

    fixed profile photo import

--- inc/integrations/pixcare_starter_content.php
+++ inc/integrations/pixcare_starter_content.php
@@ -5,27 +5,23 @@
  */
 
 /**
- * Filter the import action while getting the `noto_options` option
+ * Filter the import action while getting the `noto_options` theme_mod
  *
- * @param $option
+ * @param array $theme_mod
  *
  * @return mixed
  */
-function noto_filter_post_option_noto_options( $option ) {
-	// get the imported data
+function noto_filter_post_theme_mod_noto_options( $theme_mod ) {
+	// Get the already imported data.
 	$pixcare_options = get_option( 'pixcare_options' );
-	// this holds the ids of posts, pages, medias and everything was already imported
+	// This holds the ids of posts, pages, medias and everything was already imported.
 	$imported_options = $pixcare_options['imported_starter_content'];
 
-	// We need to replace the both logos from the demo with the imported attachment id
-	if ( isset( $imported_options['media']['ignored'][ $option['profile_photo'] ] ) ) {
-		$option['profile_photo'] = $imported_options['media']['ignored'][ $option['profile_photo'] ];
+	// We need to replace the ignored profile photo with the imported attachment id.
+	if ( isset( $imported_options['media']['ignored'][ $theme_mod['profile_photo'] ] ) ) {
+		$theme_mod['profile_photo'] = $imported_options['media']['ignored'][ $theme_mod['profile_photo'] ];
 	}
 
-	// on demo this is an option, oldy stuff
-	// but on the new installations will need this as a theme mod
-	set_theme_mod('noto_options', $option );
-
-	return $option;
+	return $theme_mod;
 }
-add_filter( 'pixcare_sce_import_post_option_noto_options', 'noto_filter_post_option_noto_options' );
+add_filter( 'pixcare_sce_import_post_theme_mod_noto_options', 'noto_filter_post_theme_mod_noto_options' );
