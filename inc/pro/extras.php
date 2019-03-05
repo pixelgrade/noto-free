a20e0dc8a171247669dd5b9db40ba5fd93b9c665
Cosmin Burloiu <cosmin@pixelgrade.com>
2019-03-05 18:04:42 +0200

    added widget areas registers in the pro folder

--- inc/pro/extras.php
+++ inc/pro/extras.php
@@ -22,6 +22,7 @@ function noto_pro_setup() {
 }
 add_action( 'after_setup_theme', 'noto_pro_setup' );
 
+
 if ( ! function_exists( 'noto_output_wave_svg' ) ) :
 	/**
 	 * Output the wave card SVG code.
@@ -33,3 +34,35 @@ if ( ! function_exists( 'noto_output_wave_svg' ) ) :
 	<?php }
 endif;
 add_action( 'pixelgrade_before_excerpt', 'noto_output_wave_svg' );
+
+
+if ( ! function_exists( 'noto_alter_blog_component_config' ) ) {
+
+	function noto_alter_blog_component_config( $config ) {
+
+		$config = Pixelgrade_Config::merge( $config, array(
+			'sidebars' => array(
+				'sidebar-1' => array(
+					'sidebar_args' => array(
+						'name'          => esc_html__( 'Posts Grid Widgets', '__theme_txtd' ),
+						'description'   => esc_html__( 'Insert your favorite widgets here, and we will place them throughout the Frontpage posts grid.', '__theme_txtd' ),
+						'before_widget' => '<div class="c-noto__item c-noto__item--widget %2$s"><section id="%1$s" class="widget">',
+						'after_widget' => '</section></div>',
+						'before_title'  => '<h2 class="widget__title h6"><span>',
+						'after_title'   => '</span></h2>',
+					),
+				),
+				'sidebar-2' => array(
+					'sidebar_args' => array(
+						'before_title' => '<h2 class="widget__title h4"><span>',
+						'after_title' => '</span></h2>',
+					),
+				)
+			)
+		) );
+
+		return $config;
+	}
+}
+	add_filter( 'pixelgrade_blog_initial_config', 'noto_alter_blog_component_config', 10 );
+
