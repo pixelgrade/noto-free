97255715c44b7e78a00dd39ef36e90f558730179
Cosmin Burloiu <cosmin@pixelgrade.com>
2019-03-11 11:09:15 +0200

    added jetpack integration to the Pro version

--- inc/pro/integrations.php
+++ inc/pro/integrations.php
@@ -14,3 +14,10 @@
  * http://pixelgrade.com/
  */
 require pixelgrade_get_parent_theme_file_path( pixelgrade_get_theme_relative_path( __DIR__ ) . 'integrations/pixelgrade-care.php' );
+
+
+/**
+ * Load Jetpack compatibility file.
+ * https://jetpack.me/
+ */
+require pixelgrade_get_parent_theme_file_path( pixelgrade_get_theme_relative_path( __DIR__ ) . 'integrations/jetpack.php' );
