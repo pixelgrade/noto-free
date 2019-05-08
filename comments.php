fa500cf3e3bb149353ba702ea670e219eb0e198f
Vlad Olaru <vlad.olaru@pixelgrade.com>
2019-05-08 15:54:19 +0300

    fix

--- comments.php
+++ comments.php
@@ -46,7 +46,7 @@ if ( post_password_required() ) {
 			<span class="c-comments-toggle__text">
 			<?php
 			/* translators: 1: the number of comments */
-			printf( esc_html( _nx( '%1$s comment', '%1$s comments', get_comments_number(), 'comments title', '__components_txtd' ) ), wp_kses( number_format_i18n( get_comments_number() ), wp_kses_allowed_html() ) );
+			printf( esc_html( _nx( '%1$s comment', '%1$s comments', get_comments_number(), 'comments title', '__theme_txtd' ) ), wp_kses( number_format_i18n( get_comments_number() ), wp_kses_allowed_html() ) );
 			?>
 			</span>
 		</label>
@@ -57,11 +57,11 @@ if ( post_password_required() ) {
 
 			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 				<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-					<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', '__components_txtd' ); ?></h2>
+					<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', '__theme_txtd' ); ?></h2>
 					<div class="nav-links">
 
-						<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', '__components_txtd' ) ); ?></div>
-						<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', '__components_txtd' ) ); ?></div>
+						<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', '__theme_txtd' ) ); ?></div>
+						<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', '__theme_txtd' ) ); ?></div>
 
 					</div><!-- .nav-links -->
 				</nav><!-- #comment-nav-above -->
@@ -82,13 +82,13 @@ if ( post_password_required() ) {
 
 			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) { // Are there comments to navigate through? ?>
 				<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
-					<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', '__components_txtd' ); ?></h2>
+					<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', '__theme_txtd' ); ?></h2>
 					<div class="nav-links">
 
 						<div
-							class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', '__components_txtd' ) ); ?></div>
+							class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', '__theme_txtd' ) ); ?></div>
 						<div
-							class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', '__components_txtd' ) ); ?></div>
+							class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', '__theme_txtd' ) ); ?></div>
 
 					</div><!-- .nav-links -->
 				</nav><!-- #comment-nav-below -->
@@ -99,15 +99,15 @@ if ( post_password_required() ) {
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) { ?>
 
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', '__components_txtd' ); ?></p>
+			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', '__theme_txtd' ); ?></p>
 			<?php
 		}
 
 		$args = array(
 			'class_form'    => 'comment-form  inputs--alt',
-			'comment_field' => '<p class="comment-form-comment"><label for="comment">' . esc_html_x( 'Comment', 'noun', '__components_txtd' ) .
+			'comment_field' => '<p class="comment-form-comment"><label for="comment">' . esc_html_x( 'Comment', 'noun', '__theme_txtd' ) .
 								'</label><textarea id="comment" class="comment__text" name="comment" cols="45" rows="8" aria-required="true"
-			                    placeholder="' . esc_html__( 'Your comment...', '__components_txtd' ) . '">' .
+			                    placeholder="' . esc_html__( 'Your comment...', '__theme_txtd' ) . '">' .
 								'</textarea></p>',
 			'submit_button' => '<button name="%1$s" type="submit" id="%2$s" class="%3$s">%4$s</button>',
 		);
