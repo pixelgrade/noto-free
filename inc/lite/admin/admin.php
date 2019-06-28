7c000c5f8e676e8a5e286b3bd37f2a27ac9e1a65
Vlad Olaru <vlad.olaru@pixelgrade.com>
2019-06-28 13:09:25 +0300

    Remove Pixelgrade Care notice

--- inc/lite/admin/admin.php
+++ inc/lite/admin/admin.php
@@ -5,16 +5,6 @@
  * @package Noto
  */
 
-function noto_lite_admin_setup() {
-
-	/**
-	 * Load and initialize Pixelgrade Care notice logic.
-	 */
-	require_once 'pixcare-notice/class-notice.php'; // phpcs:ignore
-	PixelgradeCare_Install_Notice::init();
-}
-add_action( 'after_setup_theme', 'noto_lite_admin_setup' );
-
 function noto_lite_admin_assets() {
 	wp_enqueue_style( 'noto_lite_admin_style', get_template_directory_uri() . '/inc/lite/admin/css/admin.css', array(), '1.1.2', false );
 }
