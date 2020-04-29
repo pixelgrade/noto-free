1796a1014bf47d5cbceb283d197a1d35ea4714c9
Vlad Olaru <vlad.olaru@pixelgrade.com>
2020-04-29 11:57:47 +0300

    Update starter content filter - see pixelgrade/pixelgrade-care#374

--- inc/integrations/pixcare_starter_content.php
+++ inc/integrations/pixcare_starter_content.php
@@ -8,21 +8,19 @@
  * Filter the import action while getting the `noto_options` theme_mod
  *
  * @param array $theme_mod
+ * @param string $demo_key
  *
  * @return mixed
  */
-function noto_filter_post_theme_mod_noto_options( $theme_mod ) {
-	// Get the already imported data.
-	$pixcare_options = get_option( 'pixcare_options' );
-	// This holds the ids of posts, pages, medias and everything was already imported.
-	$imported_options = $pixcare_options['imported_starter_content'];
+function noto_filter_post_theme_mod_noto_options( $theme_mod, $demo_key ) {
+	// this holds the ids of posts, pages, medias and everything was already imported
+	$starter_content = PixelgradeAssistant_Admin::get_option( 'imported_starter_content' );
 
 	// We need to replace the ignored profile photo with the imported attachment id.
-	if ( isset( $imported_options['media']['ignored'][ $theme_mod['profile_photo'] ] ) ) {
-		$theme_mod['profile_photo'] = $imported_options['media']['ignored'][ $theme_mod['profile_photo'] ];
+	if ( isset( $theme_mod['profile_photo'] ) && isset( $starter_content[ $demo_key ]['media']['ignored'][ $theme_mod['profile_photo'] ] ) ) {
+		$theme_mod['profile_photo'] = $starter_content[ $demo_key ]['media']['ignored'][ $theme_mod['profile_photo'] ];
 	}
 
 	return $theme_mod;
 }
-add_filter( 'pixcare_sce_import_post_theme_mod_noto_options', 'noto_filter_post_theme_mod_noto_options' );
-add_filter( 'pixassist_sce_import_post_theme_mod_noto_options', 'noto_filter_post_theme_mod_noto_options' );
+add_filter( 'pixassist_sce_import_post_theme_mod_noto_options', 'noto_filter_post_theme_mod_noto_options', 10, 2 );
